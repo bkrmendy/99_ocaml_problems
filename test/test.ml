5c1b98c40961975d2a99eab1c26ca80228b4c52c
Bertalan Körmendy <korber314@gmail.com>
2020-12-22 16:54:07 +0100

    addtl test cases

--- test/test.ml
+++ test/test.ml
@@ -4,13 +4,15 @@ open Lib_ocaml_problems
 let test_list_last _ =
   assert_equal None (Lib_ocaml_problems.last []);
   assert_equal (Some 1) (Lib_ocaml_problems.last [1]);
-  assert_equal (Some 2) (Lib_ocaml_problems.last [1 ; 2])
+  assert_equal (Some 2) (Lib_ocaml_problems.last [1 ; 2]);
+  assert_equal (Some "d") (Lib_ocaml_problems.last [ "a" ; "b" ; "c" ; "d" ])
 
 let test_penultimate _ = 
   assert_equal None (Lib_ocaml_problems.penultimate []);
   assert_equal None (Lib_ocaml_problems.penultimate [1]);
   assert_equal (Some 1) (Lib_ocaml_problems.penultimate [1; 2]);
-  assert_equal (Some 2) (Lib_ocaml_problems.penultimate [1; 2; 3])
+  assert_equal (Some 2) (Lib_ocaml_problems.penultimate [1; 2; 3]);
+  assert_equal (Some "c") (Lib_ocaml_problems.penultimate [ "a" ; "b" ; "c" ; "d" ])
 
 let test_kth_element _ =
   assert_equal None (Lib_ocaml_problems.kth_element [] 0);
@@ -19,22 +21,28 @@ let test_kth_element _ =
   assert_equal None (Lib_ocaml_problems.kth_element [1; 2] 2);
   assert_equal None (Lib_ocaml_problems.kth_element [1; 2] (-1));
   assert_equal (Some 1) (Lib_ocaml_problems.kth_element [1; 2] 0);
-  assert_equal (Some 2) (Lib_ocaml_problems.kth_element [1; 2] 1)
+  assert_equal (Some 2) (Lib_ocaml_problems.kth_element [1; 2] 1);
+  assert_equal (Some "c") (Lib_ocaml_problems.kth_element [ "a" ; "b"; "c"; "d"; "e" ] 2);
+  assert_equal None (Lib_ocaml_problems.kth_element [ "a" ] 3)
 
 let test_length _ = 
   assert_equal 0 (Lib_ocaml_problems.length []);
   assert_equal 1 (Lib_ocaml_problems.length [1;]);
-  assert_equal 2 (Lib_ocaml_problems.length [1; 2])
+  assert_equal 2 (Lib_ocaml_problems.length [1; 2]);
+  assert_equal 3 (Lib_ocaml_problems.length [ "a" ; "b" ; "c"])
 
 let test_reverse _ =
   assert_equal [] (Lib_ocaml_problems.reverse []);
   assert_equal [1] (Lib_ocaml_problems.reverse [1]);
-  assert_equal [2; 1] (Lib_ocaml_problems.reverse [1; 2])
+  assert_equal [2; 1] (Lib_ocaml_problems.reverse [1; 2]);
+  assert_equal ["c"; "b"; "a"] (Lib_ocaml_problems.reverse ["a" ; "b" ; "c"])
 
 let test_palindrome _ =
   assert_equal true (Lib_ocaml_problems.is_palindrome [1; 2; 1]);
   assert_equal true (Lib_ocaml_problems.is_palindrome [1; 2; 2; 1]);
-  assert_equal false (Lib_ocaml_problems.is_palindrome [1; 2; 2])
+  assert_equal true (Lib_ocaml_problems.is_palindrome [ "x" ; "a" ; "m" ; "a" ; "x" ]);
+  assert_equal false (Lib_ocaml_problems.is_palindrome [1; 2; 2]);
+  assert_equal false (Lib_ocaml_problems.is_palindrome [ "a" ; "b" ])
   
 let suite = 
   "OcamlProblemsTests" >::: [
