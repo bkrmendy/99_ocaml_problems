2fdd8d761d318f5f682a3daf6d7b5870809fe256
Bertalan Körmendy <korber314@gmail.com>
2020-12-22 16:28:05 +0100

    penultimate

--- src/Lib_ocaml_problems.ml
+++ src/Lib_ocaml_problems.ml
@@ -1,10 +1,18 @@
 module Lib_ocaml_problems : sig
   val last : 'a list -> 'a option
+  val penultimate : 'a list -> 'a option
 end = 
 struct
-let rec last lst = 
-  match lst with
-    | [] -> None
-    | (head::[]) -> Some head
-    | (_::rest) -> last rest
+    let rec last lst = 
+      match lst with
+        | [] -> None
+        | (head::[]) -> Some head
+        | (_::rest) -> last rest
+
+    let rec penultimate lst =
+      match lst with
+        | [] -> None
+        | (_::[]) -> None
+        | (pen::_::[]) -> Some pen
+        | (_::rest) -> penultimate rest
 end
--- test/test.ml
+++ test/test.ml
@@ -5,10 +5,17 @@ let test_list_last _ =
   assert_equal None (Lib_ocaml_problems.last []);
   assert_equal (Some 1) (Lib_ocaml_problems.last [1]);
   assert_equal (Some 2) (Lib_ocaml_problems.last [1 ; 2])
+
+let test_penultimate _ = 
+  assert_equal None (Lib_ocaml_problems.penultimate []);
+  assert_equal None (Lib_ocaml_problems.penultimate [1]);
+  assert_equal (Some 1) (Lib_ocaml_problems.penultimate [1; 2]);
+  assert_equal (Some 2) (Lib_ocaml_problems.penultimate [1; 2; 3])
   
 let suite = 
   "OcamlProblemsTests" >::: [
     "test_list_last" >:: test_list_last;
+    "test_penultimate" >:: test_penultimate
   ]
 
 let () = run_test_tt_main suite
